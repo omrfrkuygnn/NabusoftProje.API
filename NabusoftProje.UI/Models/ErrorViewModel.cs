4677e6c8699a4681d5c7b06da588e0c2bfba89dd
Ömer Faruk UYĞUN <uygunomerfaruk147@gmail.com>
2025-07-24 14:50:33 +0300

    Add project files.

---
+++ NabusoftProje.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NabusoftProje.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
